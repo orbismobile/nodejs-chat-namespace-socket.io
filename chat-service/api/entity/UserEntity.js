76af62734ed98ed2608f3cbbe9bf79cc0dd376d9
Carlos Leonardo Camilo <carlosleonardovargashuaman@gmail.com>
2017-04-12 14:48:12 -0500

    wip(UserEntity)

--- chat-service/api/entity/UserEntity.js
+++ chat-service/api/entity/UserEntity.js
@@ -11,28 +11,60 @@ class UserEntity {
     }
 
     addUser(userName) {
+        let that = this;
         return new Promise((resolve, reject)=> {
-            this.connection.query('CALL sp_PostUser(?);', [userName], function (error, result, fields) {
-                if (error) throw error;
+            console.log("carlos post " + this.connection);
 
-                console.log(result.affectedRows);
-                if (result.affectedRows == 1) {
-                    resolve({
-                        status: "SUCCESS",
-                        message: "User Inserted"
-                    });
-                } else {
-                    resolve({
-                        status: "ERROR",
-                        message: "An error happened"
-                    });
+            this.connection.beginTransaction(function (err) {
+
+                console.log("carlos2 " + that.connection);
+                if (err) {
+                    throw err;
                 }
+
+                that.connection.query('CALL sp_GetUserByNickname(?);', userName, function (err, result) {
+                    if (err) {
+                        return that.connection.rollback(function () {
+                            throw err;
+                        });
+                    }
+
+                    if (result[0].length == 0) {
+
+                        that.connection.query('CALL sp_PostUser(?);', [userName], function (error, result, fields) {
+                            if (error) throw error;
+
+                            console.log(result.affectedRows);
+                            if (result.affectedRows == 1) {
+                                resolve({
+                                    status: "SUCCESS",
+                                    message: "User added successful"
+                                });
+                            } else {
+                                resolve({
+                                    status: "ERROR",
+                                    message: "An error happened"
+                                });
+                            }
+                        });
+
+                    } else {
+                        resolve({
+                            status: "SUCCESS",
+                            message: "User already exist "
+                        });
+
+                    }
+
+                });
             });
         });
     }
 
     getUser() {
         return new Promise((resolve, reject)=> {
+
+            console.log("veamos get " + this.connection);
             this.connection.query('CALL sp_GetUser();', function (err, rows, fields) {
                 if (err) throw err;
 
@@ -96,7 +128,7 @@ class UserEntity {
         });
     }
 
-    getUserExceptItself(userId){
+    getUserExceptItself(userId) {
         return new Promise((resolve, reject)=> {
             this.connection.query('CALL sp_GetUserExceptItself(?);', [userId], function (err, rows, fields) {
                 if (err) throw err;
